23b5b7d70466c6b5d5cdca596067879b2f6a1887
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-11-16 00:23:51 -0500

    Simple API to think about

--- plotly_app.py
+++ plotly_app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+#@app.route("/")
+api.add_resource(HelloWorld,'/')
+
+if __name__=="__main__":
+    app.run(debug=True)
+#def hello_world():
+#    return "<p>Hello, World!</p>"
 
