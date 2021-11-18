284d3cd0381f312fa0068df9c9beefddea9b6946
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-11-17 23:18:47 -0500

    Adding some examples.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rest Api with Flask used to represent data with Plotly.js
+Use curl "http://127.0.0.1:5000" -d "data={"x":[1,2,3,4]}" -X PUT
--- plotly_app.py
+++ plotly_app.py
@@ -1,18 +1,33 @@
-from flask import Flask
+import flask
+from flask import Flask, request, render_template, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+graphs = []
 
-#@app.route("/")
-api.add_resource(HelloWorld,'/')
+#class Graph(Resource):
+#    def get(self, label):
+#        return {label: graphs[label]}
 
+#    def put(self, label):
+#        graphs[label] = request.form['data']
+#        return {label: graphs[label]}
+
+#api.add_resource(TodoSimple, '/<string:graph_id>')
+@app.route("/", methods=["GET"])
+def get_data():
+    print("graphs from get", graphs)
+    return flask.render_template("graph.html", graphs = graphs)
+
+@app.route("/", methods=["PUT","POST"])
+def put_data():
+    #print(request)
+    graph = request.form['data']
+    print(graph)
+    #graphs.append(graph.data)    
+    return jsonify(graph)
+ 
 if __name__=="__main__":
     app.run(debug=True)
-#def hello_world():
-#    return "<p>Hello, World!</p>"
-
