686aa760bd76a2157e8d5a95e8759d9dc0cb5f54
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-11-26 00:05:24 -0500

    Added adding to the specific graph data.

--- plotly_app.py
+++ plotly_app.py
@@ -17,12 +17,16 @@ graphs = []
 #        return {label: graphs[label]}
 
 
-graphs = []
+graphs = {}
 @app.route("/", methods=["GET"])
 def get_data():
-    graphs=[{"x":[1,2,3],"y":[10,9,8],"mode": "Scatter", "name":"test"},{"x":[10,12,13],"y":[1,2,3],"name":"test2","mode":"Scatter"}]    
+    #graphs=[{"x":[1,2,3],"y":[10,9,8],"mode": "Scatter", "name":"test"},{"x":[10,12,13],"y":[1,2,3],"name":"test2","mode":"Scatter"}]    
+    for name in graphs:
+        print(graphs[name])
+    html_graphs = [{"x": graphs[name]["x"],"y": graphs[name]["y"], "name": name } for name in graphs]
+    print("html_graphs=", html_graphs)
 
-    return flask.render_template("graph.html", graphs = graphs)
+    return flask.render_template("graph.html", graphs=html_graphs)
 
 @app.route("/", methods=["POST"])
 def put_data():
@@ -31,8 +35,15 @@ def put_data():
     #graph = json.loads(graph)
     graph = request.get_json()
     print(graph)
-    #graphs.append(graph)
-    return flask.render_template("graph.html", graphs = graphs)
+    for name in graph:
+        if name not in graphs:
+            graphs[name]={"x":[], "y":[]}    
+        graphs[name]["x"].extend(graph[name]["x"])
+        graphs[name]["y"].extend(graph[name]["y"])
+
+    html_graphs = [{"x": graphs[name]["x"],"y": graphs[name]["y"], "name": name } for name in graphs]
+    print("html_graphs=", html_graphs)
+    return flask.render_template("graph.html", graphs=html_graphs)
  
 if __name__=="__main__":
     app.run(debug=True)
